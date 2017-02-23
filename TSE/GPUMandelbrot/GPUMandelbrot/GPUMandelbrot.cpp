f0fe4a90d990755d5a4e1e94980a4d91e4800ebb
corruptpony <tpj_ackermans@hotmail.com>
2017-02-23 14:19:58 +0100

    alles werkt

--- TSE/GPUMandelbrot/GPUMandelbrot/GPUMandelbrot.cpp
+++ TSE/GPUMandelbrot/GPUMandelbrot/GPUMandelbrot.cpp
@@ -1,183 +1,203 @@
-// OpenCL_1.cpp : Defines the entry point for the console application.
-//
+#define _CRT_SECURE_NO_WARNINGS
+#define _SCL_SECURE_NO_WARNINGS
 
-#include "stdafx.h"
+#include "bitmap_image.hpp"
+#include "mandelbrot_frame.h"
+#include "opencl_utils.h"
+#include <windows.h>
 #include <stdio.h>
 #include <iostream>
 #include <CL/cl.h>
 #include <stdlib.h>
-#include <windows.h>
-#include "opencl_utils.h"
-#include "mandelbrot_frame.h"
-#include "bitmap_image.hpp"
-
-
-#define MEM_SIZE (128)
-#define MAX_SOURCE_SIZE (0x100000)
 
 #define WIDTH 800
 #define HEIGHT 600
 #define OFFSET_X 0
 #define OFFSET_Y 0
 #define ZOOMFACTOR 200
-#define MAX_ITERATIONS 8
-#define COLORTABLE_SIZE 1024 
+#define MAX_ITERATIONS 1024
+#define COLORTABLE_SIZE 1024
 
+mandelbrot_color colortable2[COLORTABLE_SIZE];
 
-int main(void)
+void create_colortable() 
 {
+	/* Initialize color table values */
+	for(unsigned int i = 0; i < COLORTABLE_SIZE; i++)
+	{
+		if (i < 64) {
+			mandelbrot_color color_entry = {0, 0, (5*i+20<255)?5*i+20:255};
+			colortable2[i] = color_entry;
+		}
+
+		else if (i < 128) {
+			mandelbrot_color color_entry = {0, 2*i, 255};
+			colortable2[i] = color_entry;
+		}
+
+		else if (i < 512) {
+			mandelbrot_color color_entry = {0, (i/4<255)?i/4:255, (i/4<255)?i/4:255};
+			colortable2[i] = color_entry;
+		}
+
+		else if (i < 768) {
+			mandelbrot_color color_entry = {0, (i/4<255)?i/4:255, (i/4<255)?i/4:255};
+			colortable2[i] = color_entry;
+		}
+
+		else {
+			mandelbrot_color color_entry = {0,(i/10<255)?i/10:255,(i/10<255)?i/10:255};
+			colortable2[i] = color_entry;
+		}
+	}	
+}
+
+int main() {
+	/* Define GPU parameters */
 	cl_device_id device_id = NULL;
 	cl_context context = NULL;
 	cl_command_queue command_queue = NULL;
-	cl_mem memobj = NULL;
 	cl_program program = NULL;
 	cl_kernel kernel = NULL;
 	cl_platform_id platform_id = NULL;
+	
 	cl_uint ret_num_devices;
 	cl_uint ret_num_platforms;
 	cl_int ret;
-	cl_mem PARAMSonDevice = NULL;
-	cl_mem colortableOnDevice = NULL;
-	cl_mem bufferOnDevice = NULL;
+	
+	cl_mem dev_bitmap = NULL;
+	cl_mem dev_colortable = NULL;
+	cl_mem dev_params = NULL;
 
-	#define SIZE 3
+	size_t infoSize;
 
-	remove("fractal_output.bmp");
-
-	// Create the colortable and fill it with colors
-		//create_colortable();
-
-	// Create an empty image
-	bitmap_image image(WIDTH, HEIGHT);
-	mandelbrot_color * framebuffer = (mandelbrot_color *)image.data();
-
-	/* Allocate host memory*/
-	unsigned int PARAMS_SIZE = sizeof(float) * 6;
-	unsigned int BUFFER_SIZE = sizeof(mandelbrot_color) * WIDTH * HEIGHT;
-	unsigned int COLOR_SIZE = sizeof(mandelbrot_color) * COLORTABLE_SIZE;
-	float* PARAMS = (float*)malloc(PARAMS_SIZE);
-	//mandelbrot_color* framebuffer = (mandelbrot_color*)malloc(sizeof(mandelbrot_color) * image.width * image.height);
-	mandelbrot_color* colortable = (mandelbrot_color*)malloc(COLOR_SIZE);
-
-	/* Fill PARAMS*/
-	PARAMS[0] = (float)1 / ZOOMFACTOR;
+	
+	/* Create PARAMS*/
+	unsigned int PARAMS[6];
+	PARAMS[0] = ZOOMFACTOR;
 	PARAMS[1] = WIDTH;
 	PARAMS[2] = HEIGHT;
-	PARAMS[3] = (float)OFFSET_X;
-	PARAMS[4] = (float)OFFSET_Y;
+	PARAMS[3] = OFFSET_X;
+	PARAMS[4] = OFFSET_Y;
 	PARAMS[5] = MAX_ITERATIONS;
 
-	/* Read Kernel */
-	FILE *fp;
-	char fileName[] = "./Calculate.cl";
-	char *source_str;
-	size_t source_size;
+	char* info;
+	char fileName[] = "./mandelbrot.cl";
 
-	/* Load the source code containing the kernel */
-	fp = fopen(fileName, "r");
-	if (!fp) {
-		fprintf(stderr, "Failed to load kernel.\n");
-		exit(1);
-	}
-	source_str = (char*)malloc(MAX_SOURCE_SIZE);
-	source_size = fread(source_str, 1, MAX_SOURCE_SIZE, fp);
-	fclose(fp);
+	/* Create the colortable and fill it with colors */
+	create_colortable();
 
-	printf("loaded kernel\n");
+	/* Remove previous bitmap */
+	remove("fractal_output.bmp");
+
+	/* Create an empty image */
+	bitmap_image image(WIDTH, HEIGHT);
+	mandelbrot_color * p = (mandelbrot_color *)image.data();
 
 	/* Get Platform and Device Info */
 	ret = clGetPlatformIDs(1, &platform_id, &ret_num_platforms);
+	checkError(ret, "Couldn't get platform ids");
 	ret = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_DEFAULT, 1, &device_id, &ret_num_devices);
+	checkError(ret, "Couldn't get device ids");
+	ret = clGetPlatformInfo(platform_id, CL_PLATFORM_NAME, 0, NULL, &infoSize);
+	checkError(ret, "Couldn't get platform info");
+	info = (char*)malloc(infoSize);
+	ret = clGetPlatformInfo(platform_id, CL_PLATFORM_NAME, infoSize, info, NULL);
+	checkError(ret, "Couldn't get platform attribute value");
+	printf("Running on %s\n\n", info);
 
 	/* Create OpenCL Context */
 	context = clCreateContext(NULL, 1, &device_id, NULL, NULL, &ret);
+	checkError(ret, "Couldn't create context");
 
 	/* Create command queue */
 	command_queue = clCreateCommandQueue(context, device_id, 0, &ret);
+	checkError(ret, "Couldn't create commandqueue");
 
 	/* Allocate memory for arrays on the Compute Device */
-	PARAMSonDevice = clCreateBuffer(context, CL_MEM_READ_WRITE, PARAMS_SIZE, NULL, &ret);
-	bufferOnDevice = clCreateBuffer(context, CL_MEM_READ_WRITE, BUFFER_SIZE, NULL, &ret);
-	colortableOnDevice = clCreateBuffer(context, CL_MEM_READ_WRITE, COLOR_SIZE, NULL, &ret);
+	dev_params = clCreateBuffer(context, CL_MEM_READ_ONLY, 6 * sizeof(int), NULL, &ret);
+	checkError(ret, "Couldn't create params on device");
+	dev_bitmap = clCreateBuffer(context, CL_MEM_READ_WRITE, WIDTH * HEIGHT * sizeof(mandelbrot_color), NULL, &ret);
+	checkError(ret, "Couldn't create bitmap on device");
+	dev_colortable = clCreateBuffer(context, CL_MEM_READ_ONLY, COLORTABLE_SIZE * sizeof(mandelbrot_color), NULL, &ret);
+	checkError(ret, "Couldn't create colortable on device");
 
-
-	/* Copy arrays A and B from host memory to Compute Devive */
-	ret = clEnqueueWriteBuffer(command_queue, PARAMSonDevice, CL_TRUE, 0, PARAMS_SIZE, PARAMS, 0, NULL, NULL);
-	ret = clEnqueueWriteBuffer(command_queue, colortableOnDevice, CL_TRUE, 0, COLOR_SIZE, colortable, 0, NULL, NULL);
+	/* Copy arrays from host memory to Compute Devive */
+	ret = clEnqueueWriteBuffer(command_queue, dev_params, CL_TRUE, 0, 6 * sizeof(int), PARAMS, 0, NULL, NULL);
+	checkError(ret, "Couldn't write params on device");
+	ret = clEnqueueWriteBuffer(command_queue, dev_colortable, CL_TRUE, 0, COLORTABLE_SIZE * sizeof(mandelbrot_color), colortable2, 0, NULL, NULL);
+	checkError(ret, "Couldn't write colortable on device");
 
 	/* Create kernel program */
-	program = clCreateProgramWithSource(context, 1, (const char **)&source_str, (const size_t *)&source_size, &ret);
-
-	/* Compile the program Just In Time to GPU machine code */
-	ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
+	program = build_program(context, device_id, fileName);
+	checkError(ret, "Couldn't compile");
 
 	/* Create OpenCL kernel from the compiled program */
-	kernel = clCreateKernel(program, "Calculate", &ret);
-
-	printf("kernel created\n");
+	kernel = clCreateKernel(program, "mandelbrot", &ret);
+	checkError(ret, "Couldn't create kernel");
 
 	/* Set OpenCL kernel arguments */
-	ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&PARAMSonDevice);
-	ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&bufferOnDevice);
-	ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&colortableOnDevice);
+	ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&dev_params);
+	checkError(ret, "Couldn't set arg dev_params");
+	ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&dev_bitmap);
+	checkError(ret, "Couldn't set arg dev_bitmap");
+	ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&dev_colortable);
+	checkError(ret, "Couldn't set arg dev_colortable");
 
-	// Get current time before calculating the fractal
+	/* Set global en local size */
+	size_t globalSize[] = { WIDTH , HEIGHT };
+	//size_t localSize[] = { 800 , 1 };
+
+	/* Get current time before calculating the fractal */
 	LARGE_INTEGER freq, start;
 	QueryPerformanceFrequency(&freq);
 	QueryPerformanceCounter(&start);
 
-	// Calculate the fractal
-		//mandelbrot_frame((float)OFFSET_X, (float)OFFSET_Y, (float)1/ZOOMFACTOR, MAX_ITERATIONS, p, colortable2, WIDTH, HEIGHT);
-
 	/* Activate OpenCL kernel on the Compute Device */
-	size_t globalSize[] = { BUFFER_SIZE };
-	size_t localSize[] = { 1024 };
-	clEnqueueNDRangeKernel(	command_queue, 
-							kernel, 
-							1,           // it’s a 1-dimensional matrix 
-							NULL, 
-							globalSize,  // 9 work items globally
-							localSize,   // 3 work item per group 
-							0,
-							NULL,
-							NULL);
+	ret = clEnqueueNDRangeKernel(command_queue,
+		kernel,
+		2,			// 2D matrix 
+		NULL,
+		globalSize,
+		NULL,		// local size (NULL = auto) 
+		0,
+		NULL,
+		NULL);
+	checkError(ret, "Could not activate kernel");
 
-
-	/* Transfer result array C back to host */
-	ret = clEnqueueReadBuffer(command_queue, bufferOnDevice, CL_TRUE, 0, 3 * 3 * sizeof(int), framebuffer, 0, NULL, NULL);
-
-	// Get current time after calculating the fractal
+	/* Get current time after calculating the fractal */
 	LARGE_INTEGER end;
 	QueryPerformanceCounter(&end);
 
-	// Print elapsed time
-	printf("Elapsed time to calculate fractal: %f msec\n", (double)(end.QuadPart - start.QuadPart) / freq.QuadPart * 1000.0);
-	printf("Press ENTER to continue...\n");
-	getchar();
+	/* Transfer result back to host */
+	ret = clEnqueueReadBuffer(command_queue, dev_bitmap, CL_TRUE, 0, WIDTH * HEIGHT * sizeof(mandelbrot_color), p, 0, NULL, NULL);
+	checkError(ret, "Couldn't get data form host");
 
-	// Write image to file
-	image.save_image("fractal_output.bmp");
-
-	// Show image in mspaint
-	WinExec("mspaint fractal_output.bmp", SW_MAXIMIZE);
+	/* Add blocking element */
+	clFinish(command_queue);
 
 	/* Finalization */
 	ret = clFlush(command_queue);
 	ret = clFinish(command_queue);
 	ret = clReleaseKernel(kernel);
 	ret = clReleaseProgram(program);
-	ret = clReleaseMemObject(memobj);
+	ret = clReleaseMemObject(dev_bitmap);
+	ret = clReleaseMemObject(dev_colortable);
+	ret = clReleaseMemObject(dev_params);
 	ret = clReleaseCommandQueue(command_queue);
 	ret = clReleaseContext(context);
 
-	free(source_str);
-	free(PARAMS);
-	free(colortable);
-	free(framebuffer);
+	/* Write image to file */
+	image.save_image("fractal_output.bmp");
+
+	/* Show image in mspaint */
+	WinExec("mspaint fractal_output.bmp", SW_MAXIMIZE);
+
+	/* Print elapsed time */
+	printf("Elapsed time to calculate fractal: %f msec\n", (double)(end.QuadPart - start.QuadPart) / freq.QuadPart * 1000.0);
+	printf("Press ENTER to continue...\n");
 
 	getchar();
 
 	return 0;
-
 }
-
--- TSE/GPUMandelbrot/GPUMandelbrot/ReadMe.txt
+++ TSE/GPUMandelbrot/GPUMandelbrot/ReadMe.txt
@@ -1,27 +1,27 @@
 ========================================================================
-    CONSOLE APPLICATION : OpenCL_1 Project Overview
+    CONSOLE APPLICATION : CPUMandelbrot Project Overview
 ========================================================================
 
-AppWizard has created this OpenCL_1 application for you.
+AppWizard has created this CPUMandelbrot application for you.
 
 This file contains a summary of what you will find in each of the files that
-make up your OpenCL_1 application.
+make up your CPUMandelbrot application.
 
 
-OpenCL_1.vcxproj
+CPUMandelbrot.vcxproj
     This is the main project file for VC++ projects generated using an Application Wizard.
     It contains information about the version of Visual C++ that generated the file, and
     information about the platforms, configurations, and project features selected with the
     Application Wizard.
 
-OpenCL_1.vcxproj.filters
+CPUMandelbrot.vcxproj.filters
     This is the filters file for VC++ projects generated using an Application Wizard. 
     It contains information about the association between the files in your project 
     and the filters. This association is used in the IDE to show grouping of files with
     similar extensions under a specific node (for e.g. ".cpp" files are associated with the
     "Source Files" filter).
 
-OpenCL_1.cpp
+CPUMandelbrot.cpp
     This is the main application source file.
 
 /////////////////////////////////////////////////////////////////////////////
@@ -29,7 +29,7 @@ Other standard files:
 
 StdAfx.h, StdAfx.cpp
     These files are used to build a precompiled header (PCH) file
-    named OpenCL_1.pch and a precompiled types file named StdAfx.obj.
+    named CPUMandelbrot.pch and a precompiled types file named StdAfx.obj.
 
 /////////////////////////////////////////////////////////////////////////////
 Other notes:
