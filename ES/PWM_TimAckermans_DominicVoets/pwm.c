fe0da0ac1f7c4e0012ab0d9edb2d7965ac5fdc4c
corruptpony <tpj_ackermans@hotmail.com>
2017-05-04 10:42:11 +0200

    pwm verder

--- ES/PWM_TimAckermans_DominicVoets/pwm.c
+++ ES/PWM_TimAckermans_DominicVoets/pwm.c
@@ -14,10 +14,36 @@
 #include <linux/io.h>
 #include <asm/uaccess.h>  /* for put_user */
 #include <asm/errno.h>
-//#include <mach/hardware.h>
+#include <asm/io.h>
+#include <mach/hardware.h>
 
 /* Defines */ 
-#define SUCCESS 0
+
+//#define mappedValue 0x00007D00
+
+#define PWM_CLOCK_SIGN 0x400040B8 
+#define CLOCK_ENABLE_BIT 0x0000011F
+#define LCD_BIT 0x40004054
+#define LCD_DISABLE_BIT 0x00000000
+
+#define PWM_ENABLE_BIT 0x80000000
+#define PWM_DUTY_CYCLE_BITS 0x000000FF
+#define PWM_FREQUENCY_BITS 0x0000FF00
+#define PWM_DUTY_CYCLE_END_POSITION 8
+
+#define PWM_1_CTRL 0x4005C000
+#define PWM_2_CTRL 0x4005C004
+
+#define MIN_PWM_INPUT 0
+#define MAX_PWM_INPUT 100
+#define MIN_PWM_OUTPUT 255
+#define MAX_PWM_OUTPUT 0
+
+#define MIN_FREQ_INPUT 1
+#define MAX_FREQ_INPUT 128
+#define MIN_FREQ_OUTPUT 256
+#define MAX_FREQ_OUTPUT 0
+
 #define DEVICE_NAME "pwm" /* Dev name as it appears in /proc/devices   */
 #define BUF_LEN 80        /* Max length of the message from the device */
 
@@ -30,7 +56,7 @@
 #define PWM2_DUTY 5
 
 /* Global variables */
-static int Device_Open = 0;  /* Is device open?  Used to prevent multiple access to the device */
+int Device_Open = 0;  /* Is device open?  Used to prevent multiple access to the device */
 char pwm1Enable[BUF_LEN];
 char pwm1Freq[BUF_LEN];
 char pwm1Duty[BUF_LEN];
@@ -42,6 +68,40 @@ char pwm2Duty[BUF_LEN];
 static char *msg_Ptr;
 
 int minorNr = 0;
+
+long map(long x, long in_min, long in_max, long out_min, long out_max)
+{
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+void pwm(void)
+{
+	uint32_t regValue;
+    uint32_t PWM1_regValue;
+    uint32_t offregValue;
+    int mappedValue;
+    int inputValue;
+    uint32_t mask;
+
+    //PWM1_enable:
+    regValue = PWM1_regValue | PWM_ENABLE_BIT;
+    iowrite32(regValue,io_p2v(PWM_1_CTRL));
+
+    // PWM1_duty_cycle:
+    offregValue = PWM1_regValue & ~(PWM_DUTY_CYCLE_BITS);
+    mappedValue = map(inputValue, MIN_PWM_INPUT , MAX_PWM_INPUT , MIN_PWM_OUTPUT, MAX_PWM_OUTPUT);
+
+    regValue = offregValue | (mappedValue);
+    iowrite32(regValue, io_p2v(PWM_1_CTRL));   
+    
+    // PWM1_frequency:
+    offregValue = PWM1_regValue & ~(PWM_FREQUENCY_BITS);
+    
+    mappedValue = map(inputValue, MIN_FREQ_INPUT, MAX_FREQ_INPUT, MIN_FREQ_OUTPUT, MAX_FREQ_OUTPUT);
+    mask = mappedValue << PWM_DUTY_CYCLE_END_POSITION;
+    regValue = offregValue | (mask);
+    iowrite32(regValue, io_p2v(PWM_1_CTRL));   
+}
 
 /* Called when a process tries to open the device file, like
  * "cat /dev/mycharfile"
@@ -81,7 +141,7 @@ static int device_open(struct inode *inode, struct file *fp)
 		}
 	}
 
-	return SUCCESS;
+	return 0;
 }
 
 
@@ -182,6 +242,11 @@ int __init sysfs_init(void)
 	}
 
 	 return result;
+
+	 //disable lcd
+    iowrite32(LCD_DISABLE_BIT,io_p2v(LCD_BIT));
+    //enable timer
+    iowrite32(CLOCK_ENABLE_BIT,io_p2v(PWM_CLOCK_SIGN));
 }
 
 void __exit sysfs_exit(void)
